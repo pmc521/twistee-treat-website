2dcbc8d1966f300d6f93a1b7da78e0d417a39549
Peter Corbett <pmc521@yahoo.com>
2017-07-03 15:57:42 -0400

    added inventory form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703132105) do
+ActiveRecord::Schema.define(version: 20170703142151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inventories", force: :cascade do |t|
+    t.decimal "amount", precision: 10, scale: 2, null: false
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.integer "number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
