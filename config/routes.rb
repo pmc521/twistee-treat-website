0abd787a1112d66d303a2da757d9bf682d3341ae
Peter Corbett <pmc521@yahoo.com>
2017-07-10 21:03:39 -0400

    added authentication to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,36 @@ Rails.application.routes.draw do
   get "/flavors", to: "home#flavors", as: "flavors"
   get "/prices", to: "home#prices", as: "prices"
   get "/about", to: "home#about", as: "about"
-  resources :inventories
-  resources :photos, only: [:index, :new, :create]
-  resources :products
-  resources :shifts
-  resources :schedules
+
   root 'home#show'
-  devise_for :users
-  resources :users
-  resources :simple_calendar
-  namespace :api do
-    namespace :v1 do
-      resources :users
+  devise_for :users, only: [:registrations, :sessions]
+
+  authenticate :user do
+    resources :simple_calendar
+    resources :users
+    resources :inventories  
+    resources :photos, only: [:index, :new, :create]
+    resources :products
+    resources :shifts
+    resources :schedules
+    namespace :api do
+      namespace :v1 do
+        resources :users
+      end
     end
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :posts
+    namespace :api do
+      namespace :v1 do
+        resources :posts
+      end
     end
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :comments
+    namespace :api do
+      namespace :v1 do
+        resources :comments
+      end
     end
-  end
-  resources :inventories do
-      get 'delete_all'
+    resources :inventories do
+        get 'delete_all'
+    end
   end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
